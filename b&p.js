618679c9dbb0a073be3d7dffe592a59480d85300
bridden8 <98880867+bridden8@users.noreply.github.com>
2022-03-02 16:38:51 +0300

    Add files via upload

---
+++ b&p.js
@@ -0,0 +1,18 @@
+document.getElementById("defu").value;
+function code1() {
+  var p = "coding is joyous";
+
+  document.write(p);
+}
+document.getElementById("defi").value;
+function code2() {
+  var z = "If it works don't touch it";
+
+  document.write(z);
+}
+document.getElementById("deft").value;
+function code3() {
+  var f = "coding is life";
+
+  document.write(f);
+}
