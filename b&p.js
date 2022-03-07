20c0086138b09a773160d54667c3129658e6cd90
bridden8 <98880867+bridden8@users.noreply.github.com>
2022-03-07 12:45:38 +0300

    Update b&p.js

--- b&p.js
+++ b&p.js
@@ -2,17 +2,17 @@ document.getElementById("defu").value;
 function code1() {
   var p = "coding is joyous";
 
-  document.write(p);
+  
 }
 document.getElementById("defi").value;
 function code2() {
   var z = "If it works don't touch it";
 
-  document.write(z);
+
 }
 document.getElementById("deft").value;
 function code3() {
   var f = "coding is life";
 
-  document.write(f);
+  document.write(p + z + f);
 }
